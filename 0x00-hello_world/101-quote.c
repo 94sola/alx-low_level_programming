28112d3affdc225c25519e8e64707bf3c97b65e9
94sola <ademorotiolusola@gmail.com>
2023-02-16 14:50:57 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio>
+#include<unistd.h>
 /**
- * main - A program that prints a line to be the standard error
+ * main - A c program that prints a line to the standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
+
