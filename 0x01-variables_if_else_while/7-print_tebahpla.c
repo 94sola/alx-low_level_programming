e4762b0a8f57fb0a4ff8898775f2467ddc2063f5
94sola <ademorotiolusola@gmail.com>
2023-02-19 07:12:53 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main
+ * Description: Prints lowercase alphabets in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
