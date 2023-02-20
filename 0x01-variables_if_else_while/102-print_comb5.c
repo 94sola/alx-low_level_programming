089942b51fbd449983f158b33d0291f167d5a0c8
94sola <ademorotiolusola@gmail.com>
2023-02-20 13:32:51 -0800

    Software is eating th world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,41 +7,36 @@
  */
 int main(void)
 {
-	int c, i, k, j;
+	int i, j;
+	int a, b, c, d;
 
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles fnum*/
+		b = i % 10; /* singles fnum*/
 
-	for (c 48; c <= 57; c++)
+	for (j = 0; j < 100; j++)
 	{
-	for (i 48; i <= 57; i++)
-	{
-	for (k 48; k <= 57; k++)
-	{
-	for (j 48; k <= 57; j++)
-	{
-		if (((k + j) > (C + i) && k >= c) || c < k)
-	{
-		putchar(c);
-		putchar(i);
-		putchar('');
-		putchar(k);
-		putchar(j);
+	c = j / 10; /* double snum */
+	d = j % 10; /* single snum */
 
+	if (a < c || (a == c && b < d))
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(32);
+		putchar(c + '0');
+		putchar(d + '0');
 
-		if (c + i + k + j == 16 && c == 8)
+	if (!(a == 9 && b == 8))
 	{
-		break;
-	}
-		else
-	{
-		putchar(',');
-		putchar(' ');
+		putchar(44);
+		putchar(32);
 	}
 	}
 	}
 	}
-	}
-	}
-	putchar('\n');
+	putchar(10);
 
 	return (0);
 }
